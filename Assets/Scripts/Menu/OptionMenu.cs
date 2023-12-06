b9de24cedf80c8d9e292cd3bc26ba4cea8bccd33
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2023-12-05 19:54:05 -0500

    Restart scene activated

--- Assets/Scripts/Menu/OptionMenu.cs
+++ Assets/Scripts/Menu/OptionMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OptionMenu : MonoBehaviour
 {
@@ -15,4 +16,9 @@ public class OptionMenu : MonoBehaviour
     {
         
     }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
